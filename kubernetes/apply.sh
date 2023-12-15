827447bf9d70ac188451b8c2dd3b24ccc7601fc8
Paulo Henrique Passella <passella@gmail.com>
2023-12-14 22:27:57 -0300

    Microk8s: Reformulação dos arquivos de manifestos

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
-cd postgresql
-./apply.sh
+echo "Subindo solução ..."
 
-cd ..
-kubectl apply -f namespace.yml
-kubectl apply -f storage-class.yaml
-kubectl apply -f traefik-ingress-controller.yaml
+kubectl apply -f database/tap-on-phone-database-namespace.yaml
+
+kubectl apply -f database/volume/database-storage-class.yaml
+kubectl apply -f database/volume/database-persistent-volume.yaml
+kubectl apply -f database/volume/database-persistent-volume-claim.yaml
+
+kubectl apply -f database/postgresql/postgresql-password.yaml
+kubectl apply -f database/postgresql/postgresql-stateful-set.yaml
+kubectl apply -f database/postgresql/postgresql-service.yaml
+
+kubectl apply -f database/pgpool/pgpool-stateful-set.yaml
+kubectl apply -f database/pgpool/pgpool-service.yaml
+
+echo "Porta PostgreSQL: $(kubectl get service pgpool -n tap-on-phone-database -o jsonpath='{.spec.ports[0].nodePort}')"
+echo "Solução pronta para uso"
--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-kubectl delete -f traefik-ingress-controller.yaml
+echo "Removendo solução ..."
 
-# shellcheck disable=SC2164
-cd postgresql
-./delete.sh
-# shellcheck disable=SC2103
-cd ..
+kubectl delete -f database/pgpool/pgpool-service.yaml
+kubectl delete -f database/pgpool/pgpool-stateful-set.yaml
 
-kubectl delete -f storage-class.yaml
-kubectl delete -f namespace.yml
+kubectl delete -f database/postgresql/postgresql-service.yaml
+kubectl delete -f database/postgresql/postgresql-stateful-set.yaml
+kubectl delete -f database/postgresql/postgresql-password.yaml
+
+kubectl delete -f database/volume/database-persistent-volume-claim.yaml
+kubectl delete -f database/volume/database-persistent-volume.yaml
+kubectl delete -f database/volume/database-storage-class.yaml
+
+kubectl delete -f database/tap-on-phone-database-namespace.yaml
+
+echo "Solução removida com sucesso"
